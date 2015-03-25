5eeefe8fc3de4597059a88a6bc980c4d8e0c6b6b
mrudelle <matthieu.rudelle@gmail.com>
2015-03-25 16:54:09 +0100

    chained api call to scrape entire list of user

--- star-crawler/crawler.js
+++ star-crawler/crawler.js
@@ -1,9 +1,10 @@
 
-var GITHUB_API_HOST = "api.github.com"
+var GITHUB_API_HOST = "https://api.github.com";
 
 
 var https = require('https');
-var parse = require('parse-link-header')
+var parse = require('parse-link-header');
+var urls = require('url');
 
 /**
  * Returns the json from an API call with suffix
@@ -14,28 +15,63 @@ var parse = require('parse-link-header')
  */
 var apiRequest = function (suffix, callback)
 {
-    var options = {
-        host: GITHUB_API_HOST,
-        path: suffix,
-        headers: {'user-agent': 'node.js'/*'Mozilla/5.0'*/}
-    };
 
-    https.get(options, function (response) {
+    var request = function (url, buffer, callback, apierr) {
 
-        var buffer = "";
-        var headers = response.headers;
+        var parsedUrl = urls.parse(url);
 
-        console.log(headers);
+        var options = {
+            host: parsedUrl.host,
+            path: parsedUrl.path,
+            headers: {'user-agent': 'node.js'/*'Mozilla/5.0'*/}
+        };
 
-        response.on("data", function (data) {
-            buffer += data;
-        });
 
-        response.on("end", function (err) {
-            var parsed = JSON.parse(buffer);
-            callback(parsed);
-        });
-    })
+        https.get(options, function (response) {
+
+            var buffer = "";
+            var headers = response.headers;
+
+            //console.log(headers)
+            var next = headers['link'] == undefined ? undefined : parse(headers['link'])['next'];
+
+            response.on("data", function (data) {
+                buffer += data;
+            });
+
+            response.on("end", function (err) {
+
+                // in case of an error
+                if (response.statusCode !== 200)
+                {
+                    console.log("HTTP ERROR " + response.statusCode + " for options " + JSON.stringify(options))
+                    console.log("msg: " + JSON.stringify(JSON.parse(buffer)));
+
+                    callback([]);
+                }
+                else
+                {
+                    var parsed = JSON.parse(buffer);
+
+                    // chaining api calls
+                    if (next != undefined)
+                    {
+                        console.log("will request now " + next['url']);
+                        request(next['url'], buffer.concat(parsed), callback);
+                    }
+                    else
+                    {
+                        callback(buffer.concat(parsed))
+                    }
+                }
+            });
+
+        }).on("error", function(e) {
+            console.log("Error while GET " + e.message + "\noptions:\n" + JSON.stringify(options))
+        })
+    }
+
+    request(GITHUB_API_HOST + suffix, [], callback)
 };
 
 /**
